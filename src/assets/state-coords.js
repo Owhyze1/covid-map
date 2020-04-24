a00843d1fda0ac8f1f277328191edcee070df253
Tiffany Wise <TiffanyJWise84@gmail.com>
2020-04-24 14:04:50 -0400

    Added API info
    Data from API is displayed on world map

---
+++ src/assets/state-coords.js
@@ -0,0 +1,90 @@
+// source
+// https://developers.google.com/public-data/docs/canonical/states_csv
+
+const States = {
+  Alaska     : { latitude: 63.588753, longitude: 	-154.493062},
+  Alabama    : { latitude: 32.318231, longitude: -86.902298},
+  Arkansas   : { latitude: 35.20105, longitude: -91.831833},
+  Arizona    : { latitude: 34.048928, longitude: -111.093731},
+  California : { latitude: 36.778261, longitude: -119.417932},
+  Colorado   : { latitude: 39.550051, longitude: -105.782067},
+  Connecticut: { latitude: 41.603221, longitude: -73.087749},
+  "District of Columbia" : { latitude: 38.905985, longitude: -77.033418},
+  Delaware   : { latitude: 38.910832, longitude: -75.52767},
+  Florida    : { latitude: 27.664827, longitude: -81.515754},
+  Georgia    : { latitude: 32.157435, longitude: -82.907123},
+  Hawaii     : { latitude: 19.898682, longitude: -155.665857},
+  Iowa       : { latitude: 41.878003, longitude: -93.097702},
+  Idaho      : { latitude: 44.068202, longitude: -114.742041},
+  Illinois   : { latitude: 40.633125, longitude: -89.398528},
+  Indiana    : { latitude: 40.551217, longitude: -85.602364},
+  Kansas     : { latitude: 39.011902, longitude: -98.484246},
+  Kentucky   : { latitude: 37.839333, longitude: -84.270018},
+  Louisiana  : { latitude: 31.244823, longitude: -92.145024},
+  Massachusetts
+             : { latitude: 42.407211, longitude: -71.382437},
+  Maryland   : { latitude: 39.045755, longitude: -76.651271},
+  Maine      : { latitude: 45.253783, longitude: -69.445469},
+  Michigan   : { latitude: 44.314844, longitude: -85.602364},
+  Minnesota  : { latitude: 46.729553, longitude: -94.6859},
+  Missouri   : { latitude: 37.964253, longitude: -91.831833},
+  Mississippi
+             : { latitude: 32.354668, longitude: -89.398528},
+  Montana    : { latitude: 46.879682, longitude: -110.362566},
+  "North Carolina"
+             : { latitude: 35.759573, longitude: -79.0193},
+  "North Dakota"
+             : { latitude: 47.551493, longitude: -101.002012},
+  Nebraska   : { latitude: 41.492537, longitude: -99.901813},
+  "New Hampshire"
+             : { latitude: 43.193852, longitude: -71.572395},
+  "New Jersey"
+             : { latitude: 40.058324, longitude: -74.405661},
+  "New Mexico"
+             : { latitude: 34.97273, longitude: -105.032363},
+  Nevada     : { latitude: 38.80261, longitude: -116.419389},
+  "New York" : { latitude: 43.299428, longitude: -74.217933},
+  Ohio       : { latitude: 40.417287, longitude: -82.907123},
+  Oklahoma   : { latitude: 35.007752, longitude: -97.092877},
+  Oregon     : { latitude: 43.804133, longitude: -120.554201},
+  Pennsylvania
+             : { latitude: 41.203322, longitude: -77.194525},
+  "Puerto Rico"
+             : { latitude: 18.220833, longitude: -66.590149},
+  "Rhode Island"
+             : { latitude: 41.580095, longitude: -71.477429},
+  "South Carolina"
+             : { latitude: 33.836081, longitude: -81.163725},
+  "South Dakota"
+             : { latitude: 43.969515, longitude: -99.901813},
+  Tennessee  : { latitude: 35.517491, longitude: -86.580447},
+  Texas      : { latitude: 31.968599, longitude: -99.901813},
+  Utah       : { latitude: 39.32098, longitude: -111.093731},
+  Virginia   : { latitude: 37.431573, longitude: -78.656894},
+  Vermont    : { latitude: 44.558803, longitude: -72.577841},
+  Washington : { latitude: 47.751074, longitude: -120.740139},
+  Wisconsin  : { latitude: 43.78444, longitude: -88.787868},
+  "West Virginia"
+             : { latitude: 38.597626, longitude: -80.454903},
+  Wyoming    : { latitude: 43.075968, longitude: -107.290284},
+
+
+  "USA otal": { latitude: 37.90902, longitude: -95.7129},
+  Guam       : { latitude: 13.4443 , longitude: 144.7937},
+  "Northern Mariana Islands"
+             :  { latitude: 15.0979, longitude: 145.6739},
+  "United States Virgin Islands"
+             :  { latitude: 18.3358, longitude: -64.8963},
+  "Navajo Nation"
+             :  { latitude: 36.0672, longitude: -109.1880}
+
+  //"US Military " :  { latitude: , longitude: -},
+  // Grand Princess Ship
+  // Wuhan Repatriated
+  // Diamond Princess Ship
+
+
+
+};
+
+export default States;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,21 +1,133 @@
+// Novel Covid API
+// https://corona.lmao.ninja/docs/#/Countries/get_countries
+
 import React from 'react';
 import Helmet from 'react-helmet';
+import L from 'leaflet';
+import axios from 'axios';
 
 import Layout from 'components/Layout';
-import Container from 'components/Container';
+import Map from 'components/Map';
+import States from 'assets/state-coords';
+
+const LOCATION = {
+  lat: 0,
+  lng: 0
+};
+const CENTER = [LOCATION.lat, LOCATION.lng];
+const DEFAULT_ZOOM = 2;
 
 const SecondPage = () => {
+
+  async function mapEffect({ leafletElement : map } = {}) {
+    if (!map) return;
+
+    let response;
+
+    try {
+      response = await axios.get('https://corona.lmao.ninja/states');
+    } catch (e) {
+      console.log('Error obtaining states data', e);
+      return;
+    }
+    console.log('United States', response);
+
+    const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((stateInfo = {}) => {
+        const { state } = stateInfo;
+        const { latitude, longitude } = States[state];
+
+
+
+        return {
+          type: 'Feature',
+          properties: {
+            ...stateInfo,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          }
+        }
+      })
+    }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        // let updatedFormatted;
+        let casesString;
+
+        const {
+          state,
+          cases,
+          todayCases,
+          deaths,
+          todayDeaths,
+          // tests,
+          testsPerMillion
+        } = properties
+
+
+      casesString = `${cases}`;
+      if ( cases > 1000 )
+        casesString = `${casesString.slice(0, -3)}k+`
+
+        const html =
+        `<span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${state}</h2>
+              <ul>
+                <li><strong>Total Cases:</strong> ${cases}</li>
+                <li><strong>Today's Cases:</strong> ${todayCases}</li>
+                <li><strong>Total Deaths:</strong> ${deaths}</li>
+                <li><strong>Today's Deaths:</strong> ${todayDeaths}</li>
+                <li><strong>Tests Per Million:</strong> ${testsPerMillion}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>`;
+
+          return L.marker( latlng, {
+            icon: L.divIcon({
+              className: 'icon',
+              html
+            }),
+            riseOnHover: true
+          });
+        }
+    });
+
+    geoJsonLayers.addTo(map);
+  }
+
+
+
+
+
+
+
+  const mapSettings = {
+    center: CENTER,
+    defaultBaseMap: 'OpenStreetMap',
+    zoom: DEFAULT_ZOOM,
+    mapEffect
+  };
+
   return (
     <Layout pageName="two">
       <Helmet>
-        <title>Page Two</title>
+        <title>United States</title>
       </Helmet>
-      <Container type="content" className="text-center">
-        <h1>Page Two</h1>
-        <p>Welcome to page 2</p>
-      </Container>
+      <Map {...mapSettings} />
     </Layout>
   );
-};
+}
 
 export default SecondPage;
